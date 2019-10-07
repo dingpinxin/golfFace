bf0f5b5f4df05c82755ea451c47fbaef876aa7c9
dingpinxin <dingpinxin@gmail.com>
2019-10-07 09:55:14 +0900

    menu追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'user-info-input', pathMatch: 'full' },
+  { path: '', redirectTo: 'user-menu', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'user-info-input', loadChildren: './user-info-input/user-info-input.module#UserInfoInputPageModule' },
   { path: 'check-in', loadChildren: './check-in/check-in.module#CheckInPageModule' },
   { path: 'check-in-success', loadChildren: './check-in-success/check-in-success.module#CheckInSuccessPageModule' },
+  { path: 'user-menu', loadChildren: './user-menu/user-menu.module#UserMenuPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/user-menu/user-menu.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-user-menu',
+  templateUrl: './user-menu.page.html',
+  styleUrls: ['./user-menu.page.scss'],
+})
+export class UserMenuPage implements OnInit {
+
+  constructor(
+    private navCtrl: NavController,) { }
+
+  ngOnInit() {
+  }
+
+  gotoUserInfo() {
+    this.navCtrl.navigateForward('user-info-input');
+  }
+
+  gotoCheckIn() {
+    this.navCtrl.navigateForward('check-in');
+  }
+}
